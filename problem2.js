87852fe78ea188993c0f0874ec6e2baeb6623a38
coder sifat <sifatcreator999@gmail.com>
2023-09-07 21:49:06 +0600

    algorithm problem number 1

---
+++ problem2.js
@@ -0,0 +1,18 @@
+/*
+
+1/write a function that returns the sum of two numbers .
+2/ write a function that returns the sum of all numbers regardless of # of params  
+
+Examples: 
+ For param1 = 1 and param2 = 2 , the output should be add(param1 ,param2) = 3
+
+ input/Output                                   Hints 
+[tme limit] 400ms (js)                              Arithmetic Operator 
+[input] integer param1                              Rest operator o
+                                                    ForEach()
+*/
+
+function sum(peram1,peram2){
+    return peram1 + peram2;
+}
+console.log(sum(1,2));
