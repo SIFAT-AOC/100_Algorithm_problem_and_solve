4a5463a17d99e21dd6036c4fc0f6614db1c7b4ee
coder sifat <sifatcreator999@gmail.com>
2023-09-13 23:57:46 +0600

    Algorithm problem number 5

---
+++ problem5.js
@@ -0,0 +1,15 @@
+function primeSum(prNumber){
+       let primetotal = 0;
+       for(let i=2 ; i<prNumber;i++){
+        for(let j=2;j<prNumber; j++){
+            if(i===j){
+                primetotal+=i;
+            }
+        }
+        if(i%j===0){
+            break;
+        }
+       }
+       return primetotal
+}
+console.log(primeSum(20));
